b823da19fa0f014e5e8cf1ebace25a499b65f176
Matheus Maximiliano <maximiliano.mprado@gmail.com>
2022-06-14 14:42:35 -0300

    feat: template finish

---
+++ index.js
@@ -0,0 +1,67 @@
+let isPlaying = false;
+let currentMusic = 1;
+let isShuffling = false;
+let isMuted = false;
+
+
+const myMusics = [ //SOMENTE EXEMPLO
+    {
+        id: 1,
+        name: "Baile da Serra",
+        artist: "Dj Nattan, Barrao, LIT UP",
+        song: 'baileDaSerra.mp3',
+        bg: 'baileDaSerra.png'
+    }
+]
+
+//Setar a música atual selecionada
+function setCurrentMusic(id) {
+
+}
+
+//Toggle de play e pause
+function toggleIsPlaying() {
+
+}
+
+//Começar uma música
+function playMusic() {
+
+}
+
+//Pausar uma música
+function pauseMusic() {
+
+}
+
+//Próxima Música
+function nextMusic() {
+
+}
+
+//Voltar de música
+function prevMusic() {
+
+}
+
+//Embaralhar
+function shuffling() {
+
+}
+
+//Toggle Ativar e Desativar o embaralhamento
+function toggleShuffling() {
+
+}
+
+//Mutar uma música
+function muteMusic() {
+
+}
+
+//Começar o app
+function app() {
+
+}
+
+app()
